2ea720c9884ddff548db9b2af2c3084251f4e325
reader00 <qoryamanahputra@gmail.com>
2024-05-06 06:16:48 +0700

    cpuram 1

--- src/Listener.js
+++ src/Listener.js
@@ -8,17 +8,10 @@ class Listener {
 
     async listen(message) {
         try {
-            const { playlistId, targetEmail } = JSON.parse(
-                message.content.toString()
-            );
+            const { playlistId, targetEmail } = JSON.parse(message.content.toString());
 
-            const notes = await this._playlistSongsService.getPlaylistSongs(
-                playlistId
-            );
-            const result = await this._mailSender.sendEmail(
-                targetEmail,
-                JSON.stringify(notes)
-            );
+            const songs = await this._playlistSongsService.getPlaylistSongs(playlistId);
+            const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify(songs));
             console.log(result);
         } catch (error) {
             console.error(error);
