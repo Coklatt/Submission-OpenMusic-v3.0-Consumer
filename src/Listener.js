2a553b9fb888449fff01c13deb5c5fa1959a4c8f
reader00 <qoryamanahputra@gmail.com>
2024-05-06 05:27:10 +0700

    io jenkins 3

--- src/Listener.js
+++ src/Listener.js
@@ -8,10 +8,17 @@ class Listener {
 
     async listen(message) {
         try {
-            const { playlistId, targetEmail } = JSON.parse(message.content.toString());
+            const { playlistId, targetEmail } = JSON.parse(
+                message.content.toString()
+            );
 
-            const songs = await this._playlistSongsService.getPlaylistSongs(playlistId);
-            const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify(songs));
+            const notes = await this._playlistSongsService.getPlaylistSongs(
+                playlistId
+            );
+            const result = await this._mailSender.sendEmail(
+                targetEmail,
+                JSON.stringify(notes)
+            );
             console.log(result);
         } catch (error) {
             console.error(error);
